74fada66bec7b7796c3ac7e0ca6cc03dd727b332
roman <roman@riflmedia.com>
2016-04-25 19:17:55 +0300

    add routing

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+app.all('/*', function(req, res, next) {
+    // Just send the index.html for other files to support HTML5Mode
+    res.sendFile('./public/index.html', { root: __dirname });
+});
 
 app.listen(PORT, function() {
     console.log('Server running on ' + PORT);
