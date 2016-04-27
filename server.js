071c093f218a02d98b2bf487e28533bca0237d16
roman <roman@riflmedia.com>
2016-04-27 09:24:04 +0300

    update server config

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 /**
  * Created by molot on 18.04.16.
  */
-
 var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
 var db = mongojs('shop', ['users']);
 var bodyParser = require('body-parser');
-
-
 var PORT = process.env.PORT || 3000;
-app.use(bodyParser.json());
 
+
+app.use(bodyParser.json());
 app.use(express.static('public'));
 app.post('/userRegister', function(req, res) {
     console.log(req.body);
@@ -19,18 +17,9 @@ app.post('/userRegister', function(req, res) {
         res.json(docs);
     });
 });
-
-
-
-
-app.all('/*', function(req, res, next) {
-    // Just send the index.html for other files to support HTML5Mode
+app.all('/*', function(req, res) {
     res.sendFile('./public/index.html', { root: __dirname });
 });
-
-
-
-
 app.listen(PORT, function() {
     console.log('Server running on ' + PORT);
 });
